813d1e3c9cd82dfb33129718301b85c9380959ce
Carter Tsai <hamming1@gmail.com>
2019-08-25 21:35:04 +0800

    1. add create word example 2. remove test.xlsx

--- MarkerService/WordService.cs
+++ MarkerService/WordService.cs
@@ -1,8 +1,27 @@
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
 namespace openxml.Services
 {
     public class WordService: iMarkerService
     {
-        public bool Create() {
+        public bool Create() 
+        {
+            var memoryStream = new MemoryStream();
+            using (var wordDocument = WordprocessingDocument.Create("test.docx", WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                run.AppendChild(new Text("Create text in body - CreateWordprocessingDocument"));
+            }
             return true;
         }
     }
